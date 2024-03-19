1028e60231868cc67d8483acc85b03ca29d239ea
Justin Heath <just.coder@outlook.com>
2024-03-19 15:42:44 +0000

    refactor: start project over

--- src/app/pages/appendix/_model/appendix.model.ts
+++
@@ -1,9 +0,0 @@
-export interface I_Appendix {
-  _id: string;
-  id: number;
-  reference: string;
-  link: string;
-  comments?: string;
-  type: 'Documentation' | 'Video';
-  topic: 'TypeScript' | 'Testing' | 'JavaScript' | 'Data' | 'Angular';
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://jc-development-api.herokuapp.com',
-  production: true,
-};
